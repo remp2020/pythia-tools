51ea2ea4d1255fc9ccb665f145172db619f5006f
banalytics <roman.gavuliak@gmail.com>
2019-05-12 12:26:16 +0200

    Add new columns to congif

--- conversion_prediction/utils/config.py
+++ conversion_prediction/utils/config.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 
-CATEGORICAL_COLUMNS = ['device', 'browser', 'os']
+CATEGORICAL_COLUMNS = ['device', 'browser', 'os', 'day_of_week']
 
-NUMERIC_COLUMNS_ORIGINAL = [
+NUMERIC_COLUMNS_BASE = [
     'pageview_count',
     'visit_count',
     'direct_visit_count',
@@ -17,6 +17,53 @@ NUMERIC_COLUMNS_ORIGINAL = [
     'days_since_last_active'
 ]
 
+NUMERIC_COLUMNS_FROM_JSON_FIELDS = [
+    'referer_medium_pageviews_direct',
+    'referer_medium_pageviews_email',
+    'referer_medium_pageviews_external',
+    'referer_medium_pageviews_internal',
+    'referer_medium_pageviews_search',
+    'referer_medium_pageviews_social',
+    'article_category_pageviews_',
+    'article_category_pageviews_blog',
+    'article_category_pageviews_ekonomika',
+    'article_category_pageviews_hlavna',
+    'article_category_pageviews_karikatury',
+    'article_category_pageviews_komentare',
+    'article_category_pageviews_kultura',
+    'article_category_pageviews_nezaradene',
+    'article_category_pageviews_pageview',
+    'article_category_pageviews_rodina-a-vztahy',
+    'article_category_pageviews_slovensko',
+    'article_category_pageviews_sport',
+    'article_category_pageviews_svet',
+    'article_category_pageviews_veda',
+    'article_category_pageviews_zdravie',
+    'referer_medium_pageviews_direct_count',
+    'referer_medium_pageviews_email_count',
+    'referer_medium_pageviews_external_count',
+    'referer_medium_pageviews_internal_count',
+    'referer_medium_pageviews_search_count',
+    'referer_medium_pageviews_social_count',
+    'article_category_pageviews__count',
+    'article_category_pageviews_blog_count',
+    'article_category_pageviews_ekonomika_count',
+    'article_category_pageviews_hlavna_count',
+    'article_category_pageviews_karikatury_count',
+    'article_category_pageviews_komentare_count',
+    'article_category_pageviews_kultura_count',
+    'article_category_pageviews_nezaradene_count',
+    'article_category_pageviews_pageview_count',
+    'article_category_pageviews_rodina-a-vztahy_count',
+    'article_category_pageviews_slovensko_count',
+    'article_category_pageviews_sport_count',
+    'article_category_pageviews_svet_count',
+    'article_category_pageviews_veda_count',
+    'article_category_pageviews_zdravie_count'
+]
+
+NUMERIC_COLUMNS = NUMERIC_COLUMNS_BASE + NUMERIC_COLUMNS_FROM_JSON_FIELDS
+
 NUMERIC_COLUMN_WINDOW_NAME_SUFFIXES_AND_PREFIXES = [
     ['', '_first_window_half'],
     ['', '_last_window_half'],
@@ -24,10 +71,10 @@ NUMERIC_COLUMN_WINDOW_NAME_SUFFIXES_AND_PREFIXES = [
 ]
 
 NUMERIC_COLUMNS = [
-    prefix_suffix_values[0] +  original_column + prefix_suffix_values[1] for original_column in NUMERIC_COLUMNS_ORIGINAL
+    prefix_suffix_values[0] +  original_column + prefix_suffix_values[1] for original_column in NUMERIC_COLUMNS_BASE
     for prefix_suffix_values in NUMERIC_COLUMN_WINDOW_NAME_SUFFIXES_AND_PREFIXES
     if original_column != 'days_since_last_active'
-] + NUMERIC_COLUMNS_ORIGINAL
+] + NUMERIC_COLUMNS_BASE
 
 BOOL_COLUMNS = [
     'is_desktop',
