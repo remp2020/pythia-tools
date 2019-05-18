dc653276cf257f48ab8f6293de69522b8a78ca83
rgavuliak <roman.gavuliak@gmail.com>
2019-05-18 13:09:46 +0200

    Add function for row-wise normalization

--- conversion_prediction/utils/data_transformations.py
+++ conversion_prediction/utils/data_transformations.py
@@ -1,3 +1,7 @@
+from numba import njit
+import numpy as np
+
+
 def unique_list(list_to_simplify):
     list_to_simplify = set(list_to_simplify)
     list_to_simplify = list(list_to_simplify)
@@ -5,3 +9,13 @@ def unique_list(list_to_simplify):
                         if len(element) > 0 and element != 'empty_user_id']
 
     return list_to_simplify
+
+
+@njit(parallel=True)
+def normalize_columns(data):
+    N = data.shape[0]
+    M = data.shape[1]
+    normalized_data = np.array([[data[i, j] / np.sum(data[i, :])
+                                 for j in range(M)] for i in range(N)])
+
+    return normalized_data
