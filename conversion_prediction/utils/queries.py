20f72493c58ecfc19605c70260168430577b990a
Roman Gavuliak <roman.gavuliak@npress-analysis-hel1-n2.ewhs.bntb.net>
2019-12-09 01:14:04 +0100

    print shape of retrieved data for aggregated browser days

--- conversion_prediction/utils/queries.py
+++ conversion_prediction/utils/queries.py
@@ -89,6 +89,7 @@ def get_feature_frame_via_sqlalchemy(
     full_query = full_query_negatives.union(full_query_positives)
 
     feature_frame = pd.read_sql(full_query.statement, full_query.session.bind)
+    print(feature_frame.shape)
     feature_frame.columns = [re.sub('anon_1_', '', column) for column in feature_frame.columns]
     feature_frame['is_active_on_date'] = feature_frame['is_active_on_date'].astype(bool)
     feature_frame['date'] = pd.to_datetime(feature_frame['date']).dt.date
