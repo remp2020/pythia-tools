950dca77baa7572f8394725523801a443ba8a7fe
Roman Gavuliak <roman.gavuliak@gmail.com>
2020-04-18 19:57:12 +0000

    remove deprecated argument

--- conversion_prediction/utils/bq_queries.py
+++ conversion_prediction/utils/bq_queries.py
@@ -557,8 +557,7 @@ def create_rolling_window_columns_config(
     }
 
     time_column_config = create_time_window_vs_day_of_week_combinations(
-        joined_queries,
-        [column for column in json_key_column_names if 'hours_' in column]
+        joined_queries
     )
 
     column_source_to_name_mapping.update(time_column_config)
